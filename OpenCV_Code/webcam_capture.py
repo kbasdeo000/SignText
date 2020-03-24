4370a8175a3dc1c183852dd50877a05d03243c33
Kat Chonka <katchonka@gmail.com>
2020-03-23 22:51:58 -0400

    Update webcam_capture.py: Add frame saving function

--- OpenCV_Code/webcam_capture.py
+++ OpenCV_Code/webcam_capture.py
@@ -1,7 +1,30 @@
-# Code to capture the webcame input and process it.
+# Code to capture the webcam input and process it.
 
+import os
+import shutil
 import cv2
 import numpy
+
+# Checks if there is already an output folder for the frames
+# If yes, it deletes the directory, then creates a new output folder
+# If no, creates a new output directory
+def frames_directory():
+    cur_path = os.getcwd()  # Get the current working directory
+    frm_path = '/Frames'
+    out_path = cur_path + frm_path  # New path to write the frames
+
+    # IF the new directory already exists, clear it by removing it
+    if os.path.isdir(out_path):
+        try:
+            shutil.rmtree(out_path)
+        except OSError:
+            print("Error: Directory exists & cannot be removed.")
+
+    try:
+        os.mkdir(out_path)
+        return out_path
+    except OSError:
+        print("Error: Creation of the 'frames' directory failed.")
 
 # VideoCapture object:
 # Argument = 0 because we are passing the webcam as input
@@ -18,19 +41,31 @@ end = (280, 400)        # End coordinates
 color = (255, 0, 0)     # Blue in BGR
 thickness = 3           # Line thickness
 
-# Loop while the webcam is on/capturing data
-while(cam.isOpened()):
+i = 0
+out_path = frames_directory()
+# Loop while the webcam is on/capturing data:
+while cam.isOpened():
     # Read the video and capture frame by frame:
     retval, frame = cam.read()
 
     # Draw a rectangle onto the screen:
     rectangle = cv2.rectangle(frame, start, end, color, thickness)
 
-    #if retval == True:
-        # Display the image:
-        #cv2.imshow('Video Feed', frame)
+    if retval == False:
+        break
 
-    cv2.imwrite
+    # Display the image:
+    cv2.imshow('Video Feed', frame)
+
+    # Flip the frame:
+    #frame = cv2.flip(frame, 0)
+    # Write the flipped frame:
+    #out.write(frame)
+    # out_path = '/home/kchonka/Documents/SignText/OpenCV_Code/Frames'
+    #cv2.imwrite('frame'+str(i)+'.jpg',frame)
+    cv2.imwrite(os.path.join(out_path, 'frame'+str(i)+'.jpg'), frame)
+    i+=1
+
 
     # Quits when pressing ESC
     if cv2.waitKey(1) == 27:
