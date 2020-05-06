f77fd92f0db2d6063113ebafecf1ef5ba97d82ff
Kat Chonka <katchonka@gmail.com>
2020-05-06 02:48:29 -0400

    Edit Flask code

--- signtext.py
+++ signtext.py
@@ -2,12 +2,17 @@
 # Back-end code
 
 import os
+import cv2
 from flask import Flask, request, jsonify, redirect
 from Predict_From_Trained_Model import *
+from flask_cors import CORS, cross_origin # Allows Cross Origin Resource Sharing
+from PIL import Image
 
 # initialize a flask object
 app = Flask(__name__)
-app.config["DEBUG"] = True
+CORS(app, support_credentials=True)
+#app.config["DEBUG"] = True
+app.config['CORS_HEADERS'] = 'Content-Type'
 # app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Create a directory in a known location to save image files to:
@@ -22,29 +27,41 @@ def home():
 
 
 # Translate page route:
-@app.route('/translate', methods=['GET','POST'])
+@app.route('/translate', methods=['POST'])
+@cross_origin(origin = '*')
 def get_image():
 
-    prediction = ''
+    prediction = None
 
-    if request.method == 'POST':
-        image = request.files['image']
+    #if request.method == 'POST':
+    #image = request.get_data()
+    #pil_image = open(image, 'wb')
+    file = request.form['image']
+    print(type(file))
+    print(file)
+    #img = cv2.imread(file)
 
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if image.filename == '':
-            return redirect(request.url)
+    # Get image path:
+    curr_path = os.getcwd()
+    print(curr_path)
+    image_path = curr_path + 'image.jpg'
+    #prediction = get_prediction(image_path)
 
-        if image and allowed_file(image.filename):
-            image.save(os.path.join(uploads_dir, secure_filename(image.filename)))
-            # Get image path:
-            curr_path = os.getcwd()
-            image_path = curr_path + secure_filename
+    return jsonify("yes")
 
-            prediction = get_prediction(image_path)
-            return jsonify(prediction)
+    '''
+    if image:
+        pil_image = Image.open(image)
+        return jsonify("yes")
+    '''    '''
+        image.save(os.path.join(uploads_dir, secure_filename(image.filename)))
+        # Get image path:
+        curr_path = os.getcwd()
+        image_path = curr_path + secure_filename
 
-    return jsonify(prediction)  # Default GET return
+        prediction = get_prediction(image_path)
+        return jsonify(prediction)
+        '''
 
 # app.run()
 if __name__ == '__main__':
