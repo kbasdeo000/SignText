cb9146c0e14ccce837c364ae27aeebca9904fbb6
stenistephan <36283762+stenistephan@users.noreply.github.com>
2020-05-13 11:02:39 -0400

    Update Translate.js

--- Translate.js
+++ Translate.js
@@ -1,37 +1,8 @@
 import React from 'react'
-import SearchField from "react-search-field";
 import Webcam from "react-webcam";
 import FadeIn from 'react-fade-in';
 import axios from 'axios';
 
-
-// function webcam () {
-
-//   console.log('webcam');
-// }
-
-
-// export const Translate = () => (
-//     <FadeIn>
-//       <p>
-//         .
-//       </p>
-//     <div>
-//       <center>
-//         <Webcam audio={false} height={365} width={500} />
-
-//         <textarea id="TextBox" rows="15" cols="55">
-//         Your translation will appear here.
-//         </textarea>
-//       </center>
-
-//       <center>
-//      <button type="button" class="btn btn-outline-warning btn-lg"> Start </button>
-//      <button type="button" class="btn btn-outline-warning btn-lg"> Stop </button>
-//      </center>
-//     </div>
-//     </FadeIn>
-// )
 
 const ms = require('pretty-ms')
 class Translate extends React.Component {
@@ -42,19 +13,49 @@ class Translate extends React.Component {
       isOn: false,
       start: 0,
       screenshot: null,
-      tab: 0
+      getdata: "",
+      tab: 0,
+      concat: ""
     }
     this.startTimer = this.startTimer.bind(this)
     this.stopTimer = this.stopTimer.bind(this)
     this.resetTimer = this.resetTimer.bind(this)
   }
+  
+  startTimer() {
+    this.setState({
+      isOn: true,
+      time: this.state.time,
+      start: Date.now() - this.state.time,
+      screenshot: this.state.screenshot,
+      getdata: this.state.getdata,
+      concat: this.state.concat
+    })
+    this.timer = setInterval(() => this.setState({
+      time: Date.now() - this.state.start}), 1000);
+    this.screenshot = setInterval(() => this.setState({
+        screenshot: this.webcam.getScreenshot()} ), 3000);
+    this.getdata = setInterval(() => this.setState({
+        getdata: this.getdatafunc(), concat: (this.state.concat + this.state.getdata)} ), 3000);
+  }
 
-  handleClick = () => {
-    const screenshot = this.webcam.getScreenshot();
-    this.setState({ screenshot });
+  stopTimer() {
+    this.setState({isOn: false})
+    this.setState({screenshot: null})
+    this.setState({getdata: ""})
+    clearInterval(this.timer)
+    clearInterval(this.screenshot)
+    clearInterval(this.getdata)
+  }
 
+  resetTimer() {
+    this.setState({time: 0, isOn: false, screenshot: this.state.screenshot, getdata: this.state.getdata, concat: ""})
+  }
+
+  getdatafunc() {
+    let newcomp = this;
     var bodyFormData = new FormData();
-    bodyFormData.append('image', screenshot);
+    bodyFormData.append('image', this.webcam.getScreenshot());
     axios({
         method: 'post',
         url: 'http://127.0.0.1:5000/translate',
@@ -63,7 +64,7 @@ class Translate extends React.Component {
         })
         .then(function (response) {
             console.log(response);
-            const prediction = response
+            newcomp.setState({ getdata: response.data});
         })
         .catch(function (response) {
             //handle error
@@ -71,66 +72,42 @@ class Translate extends React.Component {
         });
   }
 
-  startTimer() {
-    this.setState({
-      isOn: true,
-      time: this.state.time,
-      start: Date.now() - this.state.time
-    })
-    this.timer = setInterval(() => this.setState({
-      time: Date.now() - this.state.start
-    }), 1);
-    const screenshot = this.webcam.getScreenshot();
-    this.setState({ screenshot });
-  }
-  stopTimer() {
-    this.setState({isOn: false})
-    clearInterval(this.timer)
-  }
-  resetTimer() {
-    this.setState({time: 0, isOn: false})
-  }
-
-
-
+  
   render() {
-    let start = (this.state.time == 0) ?
+    let start = (this.state.time === 0) ?
     <button onClick={this.startTimer} type="button" class="btn btn-outline-warning btn-lg">Start</button>:
       null
-    let stop = (this.state.time == 0 || !this.state.isOn) ?
+    let stop = (this.state.time === 0 || !this.state.isOn) ?
       null :
-      <button onClick={this.stopTimer}>stop</button>
-    let resume = (this.state.time == 0 || this.state.isOn) ?
+      <button onClick={this.stopTimer} type="button" class="btn btn-outline-warning btn-lg">Stop</button>
+    let resume = (this.state.time === 0 || this.state.isOn) ?
       null :
-      <button onClick={this.startTimer}>resume</button>
-    let reset = (this.state.time == 0 || this.state.isOn) ?
+      <button onClick={this.startTimer} type="button" class="btn btn-outline-warning btn-lg">Resume</button>
+    let reset = (this.state.time === 0 || this.state.isOn) ?
       null :
-      <button onClick={this.resetTimer}>reset</button>
+      <button onClick={this.resetTimer} type="button" class="btn btn-outline-warning btn-lg">Reset</button>
+    
     return(
       <FadeIn>
       <center>
       <div>
         <Webcam
           audio={false}
-          height={365}
+          height={365} 
           width={500}
           ref={node => this.webcam = node}
         />
-        <textarea id="TextBox" rows="15" cols="15">
+        <textarea id="TextBox" rows="15" cols="55" value={this.state.concat}>
                    Your translation will appear here.
         </textarea>
         <div>
           <div className='screenshots'>
-            <div className='controls'>
-              <button onClick={this.handleClick} type="button" class="btn btn-outline-warning btn-lg">Start</button>
-              <button type="button" class="btn btn-outline-warning btn-lg"> Stop </button>
-            </div>
             {this.state.screenshot ? <img src={this.state.screenshot} height={265} width={355}/> : null}
           </div>
         </div>
       </div>
       <div>
-        <h3>timer: {ms(this.state.time)}</h3>
+        <h3>Timer: {ms(this.state.time)}</h3>
         {start}
         {resume}
         {stop}
@@ -142,48 +119,5 @@ class Translate extends React.Component {
   }
 }
 
-// class Translate extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       screenshot: null,
-//       tab: 0
-//     };
-//   }
-
-//   handleClick = () => {
-//     const screenshot = this.webcam.getScreenshot();
-//     this.setState({ screenshot });
-//   }
-
-//   render() {
-//     return (
-//       <FadeIn>
-//       <center>
-//       <div>
-//         <Webcam
-//           audio={false}
-//           height={365}
-//           width={500}
-//           ref={node => this.webcam = node}
-//         />
-//         <textarea id="TextBox" rows="15" cols="55">
-//                    Your translation will appear here.
-//         </textarea>
-//         <div>
-//           <div className='screenshots'>
-//             <div className='controls'>
-//               <button onClick={this.handleClick} type="button" class="btn btn-outline-warning btn-lg">Start</button>
-//               <button type="button" class="btn btn-outline-warning btn-lg"> Stop </button>
-//             </div>
-//             {this.state.screenshot ? <img src={this.state.screenshot} height={265} width={355}/> : null}
-//           </div>
-//         </div>
-//       </div>
-//       </center>
-//       </FadeIn>
-//     );
-//   }
-// }
 
 export { Translate};
